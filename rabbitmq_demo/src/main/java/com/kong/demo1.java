b05e08cab0fd7d9d1a0f5c6aaa73e95eacd6f4e9
kongdeqi <1141599766@qq.com>
2019-12-26 14:47:26 +0800

    提交rabbitmq_demo

---
+++ rabbitmq_demo/src/main/java/com/kong/demo1.java
@@ -0,0 +1,21 @@
+package com.kong;/**
+ * Created by xuebi on 2019/12/26.
+ */
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName demo1
+ * @Description TODO
+ * @Author kongdeqi
+ * @Date 2019/12/26 11:45
+ * @Version 1.0
+ */
+public class demo1 {
+
+    @RequestMapping
+    public void test1(BindingResult bindingResult ){
+
+    }
+}
---
+++ rabbitmq_demo/src/test/java/com/kong/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
